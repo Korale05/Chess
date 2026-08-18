937fb1ad3a357c62155c4f89f03ea5fc9f7d3ceb
Korale05 <onkarkorale03@gmail.com>
2026-08-18 08:45:13 +0530

    Fix : Removed BackendUrl Hardcoded value

--- Frontend/src/hooks/useUserInfo.tsx
+++ Frontend/src/hooks/useUserInfo.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 
-const BACKEND_URL = "ws://localhost:8080";
+const BACKEND_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
+
 export default async function useUserInfo(){
 
     const user = await axios.get(`${BACKEND_URL}/api/auth/me`,
